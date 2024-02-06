84eea5add7546dc3622b48469d10a61e897ed43b
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-06 18:22:54 +0300

    YEEEEEEEEEEEEAH

---
+++ physics.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+
+window = Tk()
+window.title("THE MONEY GO WHERE I GO🔥")
+window.geometry = "620x480"
+
+ttk.Button(window, text="BRUH")
+
+
+window.mainloop()
