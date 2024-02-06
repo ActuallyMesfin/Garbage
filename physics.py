04b1ca94a74ae40105d8ef786bb3a7169120f20f
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-06 18:28:48 +0300

    WHEN THE BELL RING BUT YO DINGALING LOWKEY HARD ASF

--- physics.py
+++ physics.py
@@ -1,12 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+import playsound
 
 
 window = Tk()
 window.title("THE MONEY GO WHERE I GO🔥")
-window.geometry = "620x480"
+window.geometry = "1000x1000"
 
-ttk.Button(window, text="BRUH")
+def dumbass():
+    print("FUCK")
+    playsound.playsound("Vine boom.mp4")
 
+stupidassfuckingbutton = Button(window, text="BRUH", command=dumbass)
+stupidassfuckingbutton.pack()
 
 window.mainloop()
