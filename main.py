fe5a6f5847240c9dbabb1cb7d6ccd430ff87a5d8
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-23 19:48:50 +0300

    SteamAPI

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#fuck you nigga
+from steam import Steam
+from decouple import config
+
+KEY = config("STEAM_API_KEY")
+steam = Steam(KEY)
+
+user = steam.users.search_user("the12thchairman")
+print(user)
