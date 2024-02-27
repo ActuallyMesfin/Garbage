5cb716f90da063616f66846d8e15d10c6eb03ef8
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-27 19:50:30 +0300

    Man it's so fucking hot I can't think straight

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ window = Tk("Hello")
 window.geometry("500x500")
 
 
+
+username = Entry(window, width=50)
+username.pack()
+
+def printinfo():
+    user = steam.users.search_user("the12thchairman")
+    label = Label(window, text=user)
+    label.pack()
+
+confirm_button = Button(window, text="Confirm", command=printinfo)
+confirm_button.pack()
+
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
 
-user = steam.users.search_user("the12thchairman")
-print(user)
+
 window.mainloop()
