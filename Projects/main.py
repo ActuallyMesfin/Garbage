55cfec81af208184a64298c77bf0cdce4457c4ab
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-21 10:03:00 +0300

    I don't know weather or not I should keep using this. Looks fun

--- Projects/main.py
+++ Projects/main.py
@@ -1,8 +1,31 @@
-from tkinter import Tk
-from tkinter import *
+# import the module
+import python_weather
 
-window = Tk()
-window.geometry("400x400")
-window.title("TF2")
-window.mainloop()
-#I don't want to do this anymore. I was watching a godot tutorial today so it doesn't count
+
+import asyncio
+import os
+
+async def getweather():
+  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+    # fetch a weather forecast from a city
+    weather = await client.get('Addis Ababa')
+    
+    # returns the current day's forecast temperature (int)
+    print(weather.temperature)
+    
+    # get the weather forecast for a few days
+    for daily in weather.daily_forecasts:
+      print(daily)
+      
+      # hourly forecasts
+      for hourly in daily.hourly_forecasts:
+        print(f' --> {hourly!r}')
+
+if __name__ == '__main__':
+  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+  # for more details
+  if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+  
+  asyncio.run(getweather())
