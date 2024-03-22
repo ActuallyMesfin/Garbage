6bd399e4528028855737895bfb3f33df00f466ca
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-22 19:33:49 +0300

    Fucks sake

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-import random
 
-list = {"123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-code = "XXXX-XXXX-XXXX"
-
-x = len(code)
-for i in range(0, x):
-    code[x] = random.choice(list)
-    x+=1
-
-print(code)
